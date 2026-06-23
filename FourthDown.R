153d3dd1aa05198a831a9db44b59e1a695c42232
BAChing2 <brenden.j.ching@gmail.com>
2026-06-22 18:20:24 -0700

    Update FourthDown.R

--- FourthDown.R
+++ FourthDown.R
@@ -1,5 +1,12 @@
 library(nflreadr)
 library(tidyverse)
-library(nflplotR)
 
 pbp <- load_pbp(2024)
+
+filtered_pbp <- pbp %>%
+  filter(down == 4) %>%
+  mutate (decision = case_when (
+    play_type %in% c("run", "pass") ~ "went_for_it",
+    play_type == "punt" ~ "punt",
+    play_type == "field_goal" ~ "field_goal",
+    TRUE ~ "other"))
