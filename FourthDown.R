f71f2b2bd61160e4569c002635f6b93221e20086
BAChing2 <brenden.j.ching@gmail.com>
2026-06-26 13:49:37 -0700

    Update FourthDown.R

--- FourthDown.R
+++ FourthDown.R
@@ -28,3 +28,33 @@ situations <- filtered_pbp %>%
   )) %>%
   group_by(distance) %>%
   summarize(go_rate = mean(decision == "went_for_it"))
+
+team_by_distance <- filtered_pbp %>%
+  filter(decision != "other") %>%
+  mutate(distance = case_when (
+    ydstogo <= 2 ~ "short",
+    ydstogo <= 5 ~ "medium",
+    TRUE ~ "long"
+  )) %>%
+  group_by(posteam, distance) %>%
+  summarize(
+    go_rate = mean(decision == "went_for_it"),
+    n = n(),
+    .groups = "drop"
+  ) 
+
+team_by_short_distance <- filtered_pbp %>%
+  filter(decision != "other") %>%
+  mutate(distance = case_when (
+    ydstogo <= 2 ~ "short",
+    ydstogo <= 5 ~ "medium",
+    TRUE ~ "long"
+  )) %>%
+  group_by(posteam, distance) %>%
+  summarize(
+    go_rate = mean(decision == "went_for_it"),
+    n = n(),
+    .groups = "drop"
+  ) %>%
+  filter(distance == "short") %>%
+  arrange(desc(go_rate))
